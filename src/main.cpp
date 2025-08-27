4fe615d9a0116caf46263854a73fe31aebe80db4
operator-watson <ajhawker@live.com>
2025-08-27 11:09:57 +0000

    Small cleanups

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
   // Create chat application instance
-  LlamaWrapper lw("models/L3.1-DpHermes-Rsnng-8B-DarkIdol-Inst-1.2-Uncen-D_AU-Q4_k_m.gguf");
+  LlamaWrapper lw("models/l3.1-dark-reasoning-lewdplay-evo-hermes-r1-uncensored-8b-q4_k_m.gguf");
 
   // Optional: customize configuration
   SamplingConfig samplingConfig;
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
   samplingConfig.topK = 80;
   lw.setSamplingConfig(samplingConfig);
 
-  ModelConfig modelConfig("models/L3.1-DpHermes-Rsnng-8B-DarkIdol-Inst-1.2-Uncen-D_AU-Q4_k_m.gguf");
+  ModelConfig modelConfig("models/l3.1-dark-reasoning-lewdplay-evo-hermes-r1-uncensored-8b-q4_k_m.gguf");
   modelConfig.systemMessagePath = "";
   modelConfig.nGpuLayers = 100;
   modelConfig.nCtx = 16384;
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
     // Run the interactive chat loop
     lw.runChatLoop();
 
-/*   std::string response = lw.loadFileAsFirstMessageWithResponse("prompt.txt");
+/*   std::string response = lw.loadFileAsFirstMessageWithResponse("sex_prompt.txt");
   if (!response.empty())
   {
     // File was processed and response generated
